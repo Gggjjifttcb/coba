98a55a7446d0fc1273b0e2312d3cec4e01a56e19
unknown <myogaapriadi43@gmail.com>
2026-02-09 10:04:44 +0800

    belajar css

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const cards = document.querySelectorAll(".card");
+    cards.forEach((card, i) => {
+        card.style.opacity = 0;
+        setTimeout(() => {
+            card.style.transition = "0.6s";
+            card.style.opacity = 1;
+            card.style.transform = "translateY(0)";
+        }, i * 200);
+    });
+});
